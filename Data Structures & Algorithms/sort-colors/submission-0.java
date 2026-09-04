ca996e4c306fd32c63f8bda6ff3707df5c49d87f
Jakub Pająk <45770166+iSpydeer@users.noreply.github.com>
2026-09-04 11:12:28 +0200

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] bucket = {0, 0, 0};
+        
+        for (int i=0; i < nums.length; i++) {
+            bucket[nums[i]] += 1;
+        }
+
+        int idx = 0;
+        for (int i = 0; i < bucket.length; i++) {
+            for (int j = 0; j < bucket[i]; j++) {
+                nums[idx] = i;
+                idx++;
+            }
+        }
+    }
+}
