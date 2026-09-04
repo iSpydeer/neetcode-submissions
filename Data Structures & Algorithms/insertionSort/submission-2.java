ddfd9f6e3f20ca8e3fb4de2fa3f09baa24fb4545
Jakub Pająk <45770166+iSpydeer@users.noreply.github.com>
2026-09-04 20:21:12 +0200

    Add: insertionSort - submission-2

---
+++ Data Structures & Algorithms/insertionSort/submission-2.java
@@ -0,0 +1,34 @@
+// Definition for a pair
+// class Pair {
+//     int key;
+//     String value;
+//
+//     Pair(int key, String value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+public class Solution {
+    public List<List<Pair>> insertionSort(List<Pair> pairs) {
+        List<List<Pair>> retList = new ArrayList<>();
+        int n = pairs.size();
+        if (n == 0) {
+            return retList;
+        }
+
+        retList.add(new ArrayList<>(pairs));
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j-1 >= 0 && pairs.get(j-1).key > pairs.get(j).key) {
+                Pair temp = pairs.get(j-1);
+                pairs.set(j-1, pairs.get(j));
+                pairs.set(j, temp);
+                j--;
+            }
+
+        retList.add(new ArrayList<>(pairs));
+        }
+        
+        return retList;
+    }
+}
